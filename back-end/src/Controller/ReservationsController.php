7d7c75f9771976079a5a057080e7cbfc6ef87258
Massimo Frascati <mfrascati@gmail.com>
2019-04-30 16:36:01 +0200

    Implementa multiActions su reservations

--- back-end/src/Controller/ReservationsController.php
+++ back-end/src/Controller/ReservationsController.php
@@ -46,4 +46,69 @@ class ReservationsController extends AppController
     public function _entityQuery($query, $id){
         return $query;
     }
+
+    /**
+     * Punto di ingresso per tutte le funzioni che agiscono massivamente su più record
+     * Accetta in POST
+     * - action e chiama una funzione protected con lo stesso nome
+     * - elenco di ids
+     * - params[]
+     * @return void
+     */
+    public function multiActions()
+    {
+        $this->requireFields(['action', 'ids']);
+
+        $action = \Cake\Utility\Inflector::camelize($this->request->getData('action'));
+        $ids = $this->request->getData('ids');
+
+        $res = [0 => 0, 1 => 0];
+
+        foreach($ids as $id)
+        {
+            $r = $this->Reservations->find()->where(['id' => $id])->first();
+            if($this->{'_mact_'.$action}($r, $this->request->getData('params')))
+                $res[1]++;
+            else
+                $res[0]++;
+        }
+
+        if(empty($res[1]))
+            throw new WarningException("Non è stato processato nessun elemento: $res[0] errori");
+
+        $ret = ['message' => sprintf('Processati %d elementi, %d errori', $res[1], $res[0])];
+
+        $this->_setJson(true, $ret);
+    }
+
+    /**
+     * Multiaction child
+     * Assegna più prenotazioni a una stanza
+     * @return boolean
+     */
+    protected function _mact_assignRoom($r, $params = [])
+    {
+        if(empty($params['room_id']))
+            throw new WarningException("Sotto params manca 'room_id'");
+
+        $r->room_id = $params['room_id'];
+        return $this->Reservations->save($r);
+    }
+
+    /**
+     * Multiaction child
+     * Effettua o revoca il check in/out di una prenotazione
+     * @return boolean
+     */
+    protected function _mact_check($r, $params = [])
+    {
+        if(empty($params['type']) || !in_array($params['type'], ['in', 'out']))
+            throw new WarningException("Sotto params manca 'type' ('in'/'out')");
+
+        if(empty($params['value']))
+            throw new WarningException("Sotto params manca 'value' (Date/null)");
+
+        $r->{'data_'.$params['type']} = $params['value'];
+        return $this->Reservations->save($r);
+    }
 }
