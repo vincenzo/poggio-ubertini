4236e80fabacf378b1e840041672a9455e5a32d2
Elia <eliagentili@gmail.com>
2019-11-22 13:52:38 +0100

    Fix

--- front-end/src/app/components/rooms-calendario/rooms-calendario.controller.ts
+++ front-end/src/app/components/rooms-calendario/rooms-calendario.controller.ts
@@ -30,7 +30,7 @@ export class RoomsCalendarioComponentController {
 
   private _mapStateToThis(state) {
     return this.RoomsCalendarioService.mapStateToThisIndex({
-      calendario: state.calendario.toJS(),
+      calendario: state.toJS().calendario,
     })(state);
   }
 
