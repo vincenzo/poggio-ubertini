6df075fede634ff3a535063e1c155964294d5ab0
Elia <eliagentili@gmail.com>
2019-11-22 12:30:29 +0100

    Crea index calendario

---
+++ front-end/src/app/components/rooms-calendario/rooms-calendario.component.ts
@@ -0,0 +1,8 @@
+import { RoomsCalendarioComponentController } from './rooms-calendario.controller';
+
+import './rooms-calendario.scss';
+
+export const RoomsCalendarioComponent: ng.IComponentOptions = {
+  controller: RoomsCalendarioComponentController,
+  templateUrl: require('./rooms-calendario.html'),
+}
---
+++ front-end/src/app/components/rooms-calendario/rooms-calendario.module.ts
@@ -0,0 +1,36 @@
+import * as angular from "angular";
+
+import { RoomsCalendarioComponent } from "./rooms-calendario.component";
+
+import { AppService } from "../../common/app/app.service";
+import { RoomsCalendarioService } from "./rooms-calendario.service";
+import { StateProvider } from "@uirouter/angularjs";
+
+export const RoomsCalendarioModule = angular
+  .module("components.rooms-calendario", ["ui.router"])
+  .config(($stateProvider: StateProvider) => {
+    "ngInject";
+
+    $stateProvider.state("roomsCalendario", {
+      parent: "app",
+      url: "/rooms-calendario",
+      component: "roomsCalendario",
+      resolve: {
+        roomsCalendario: (
+          $ngRedux,
+          RoomsCalendarioService: RoomsCalendarioService
+        ) => {
+          "ngInject";
+          return $ngRedux.dispatch(RoomsCalendarioService.getCalendario(2019));
+        }
+      },
+      data: {
+        requiredAuth: true,
+        roles: {
+          admin: true
+        }
+      }
+    });
+  })
+  .component("roomsCalendario", RoomsCalendarioComponent)
+  .service("RoomsCalendarioService", RoomsCalendarioService).name;
