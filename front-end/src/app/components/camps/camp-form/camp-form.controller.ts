192f3d9016f4ac1c0822891ea260a1c82d979e27
Elia <eliagentili@gmail.com>
2020-02-16 17:37:49 +0100

    Redirect a view after add

--- front-end/src/app/components/camps/camp-form/camp-form.controller.ts
+++ front-end/src/app/components/camps/camp-form/camp-form.controller.ts
@@ -1,13 +1,12 @@
-import * as jQuery from 'jquery';
-import ngRedux from 'ng-redux';
-import { scrollToElement } from '../../../vendors/ew-angularjs-utils/utils/scroll-to-top';
-import { UploadsService } from '../../../vendors/ew-angularjs-utils/components/uploads/uploads.service';
-import { stateGo } from 'redux-ui-router';
+import * as jQuery from "jquery";
+import ngRedux from "ng-redux";
+import { scrollToElement } from "../../../vendors/ew-angularjs-utils/utils/scroll-to-top";
+import { UploadsService } from "../../../vendors/ew-angularjs-utils/components/uploads/uploads.service";
+import { stateGo } from "redux-ui-router";
 
-import { CampsService } from '../camps.service';
+import { CampsService } from "../camps.service";
 
 export class CampFormComponentController {
-
   auth: any;
   action: string;
   activeTab: number;
@@ -33,9 +32,9 @@ export class CampFormComponentController {
     private $timeout: ng.ITimeoutService,
     private hotkeys,
     private CampsService: CampsService,
-    private UploadsService: UploadsService,
+    private UploadsService: UploadsService
   ) {
-    'ngInject';
+    "ngInject";
     this.unsubscribe = this.$ngRedux.connect(
       this._mapStateToThis.bind(this),
       this._mapDispatchToThis
@@ -43,19 +42,43 @@ export class CampFormComponentController {
   }
 
   $onInit() {
-    this.title = (this.isEdit() ? 'Modifica ' : 'Aggiungi ') + 'campo';
+    this.title = (this.isEdit() ? "Modifica " : "Aggiungi ") + "campo";
 
     // Seleziona di default il primo tab
     this.activeTab = 0;
-    this.tabs = ['doc_offerta', 'emails', 'doc_renter'];
+    this.tabs = ["doc_offerta", "emails", "doc_renter"];
     this.tabs.forEach((tab, index) => this._addTabsHotkeys(tab, index));
 
     // FIXME: rimuovere dopo test
     this.mockGuests = [
-      { id: 1, nome: 'Elia', cognome: 'Gentili', arrivo: new Date(), partenza: new Date() },
-      { id: 2, nome: 'Massimo', cognome: 'Frascati', arrivo: new Date(), partenza: new Date() },
-      { id: 3, nome: 'Michele', cognome: 'Spina', arrivo: new Date(), partenza: new Date() },
-      { id: 4, nome: 'Luca', cognome: 'Bottero', arrivo: new Date(), partenza: new Date() },
+      {
+        id: 1,
+        nome: "Elia",
+        cognome: "Gentili",
+        arrivo: new Date(),
+        partenza: new Date()
+      },
+      {
+        id: 2,
+        nome: "Massimo",
+        cognome: "Frascati",
+        arrivo: new Date(),
+        partenza: new Date()
+      },
+      {
+        id: 3,
+        nome: "Michele",
+        cognome: "Spina",
+        arrivo: new Date(),
+        partenza: new Date()
+      },
+      {
+        id: 4,
+        nome: "Luca",
+        cognome: "Bottero",
+        arrivo: new Date(),
+        partenza: new Date()
+      }
     ];
   }
 
@@ -65,20 +88,20 @@ export class CampFormComponentController {
   }
 
   isAdd() {
-    return this.action === 'add';
+    return this.action === "add";
   }
 
   isAuthorized(element: string) {
     switch (element) {
-      case 'ipotesiSpesa':
-        return ['admin'].indexOf(this.auth.user.role) > -1;
+      case "ipotesiSpesa":
+        return ["admin"].indexOf(this.auth.user.role) > -1;
       default:
         return false;
     }
   }
 
   isEdit() {
-    return this.action === 'edit';
+    return this.action === "edit";
   }
 
   onUploadFile(event) {
@@ -86,13 +109,16 @@ export class CampFormComponentController {
   }
 
   reloadFormData() {
-    return this.updateModel({ name: 'upload', value: null, skipHash: true })
-      .then(() => this.getFormData(this.model.id));
+    return this.updateModel({
+      name: "upload",
+      value: null,
+      skipHash: true
+    }).then(() => this.getFormData(this.model.id));
   }
 
   selectTab(tabName: string) {
     this.$timeout(() => {
-      jQuery('#focused_input_' + tabName).focus();
+      jQuery("#focused_input_" + tabName).focus();
       // this.$scope.$broadcast('SetFocus');
     }, 50);
   }
@@ -104,18 +130,30 @@ export class CampFormComponentController {
   submit(model, stay) {
     if (this.form.$valid) {
       let response = null;
-      return this.save(model)
-        .then((res) => response = res.value.data)
-        .then(() => this.get())
-        // se clicco salva, rimando sempre alla index
-        .then(() => !stay ? this.stateGo('camps') : null)
-        // se clicco salva e resta ho 2 casi: 1- sono in add e quindi rimando alla edit, 2- sono in edit e quindi resto dove sono
-        .then(() => stay && this.isAdd() ? this.stateGo('camps.edit', ({ id: response.id })) : null)
-        .then(() => this.CampsService.toaster.success('Campo salvato'));
+      return (
+        this.save(model)
+          .then(res => (response = res.value.data))
+          .then(() => this.get())
+          // se clicco salva, rimando sempre alla index
+          .then(() =>
+            !stay ? this.stateGo("camps.view", { id: response.id }) : null
+          )
+          // se clicco salva e resta ho 2 casi: 1- sono in add e quindi rimando alla edit, 2- sono in edit e quindi resto dove sono
+          .then(() =>
+            stay && this.isAdd()
+              ? this.stateGo("camps.edit", { id: response.id })
+              : null
+          )
+          .then(() => this.CampsService.toaster.success("Campo salvato"))
+      );
     }
 
-    this.CampsService.toaster.error('Sono presenti degli errori. Controlla e riprova.');
-    const el: any = jQuery('#campform-dialog input.ng-invalid:first, select.ng-invalid:first');
+    this.CampsService.toaster.error(
+      "Sono presenti degli errori. Controlla e riprova."
+    );
+    const el: any = jQuery(
+      "#campform-dialog input.ng-invalid:first, select.ng-invalid:first"
+    );
     el.focus();
     scrollToElement(el, 80);
   }
@@ -123,7 +161,7 @@ export class CampFormComponentController {
   updateCapogruppo(event) {
     return this.updateModel({
       name: event.name,
-      value: event.value.value,
+      value: event.value.value
     });
   }
 
@@ -135,8 +173,8 @@ export class CampFormComponentController {
     this.hotkeys.add({
       combo: [`ctrl+${index + 1}`, `mod+${index + 1}`],
       description: `Seleziona tab "${tab}"`,
-      allowIn: ['INPUT', 'SELECT', 'TEXTAREA'],
-      callback: (e) => {
+      allowIn: ["INPUT", "SELECT", "TEXTAREA"],
+      callback: e => {
         e.preventDefault();
         this.activeTab = index;
       }
@@ -153,12 +191,11 @@ export class CampFormComponentController {
 
   private _mapDispatchToThis = dispatch => {
     return this.CampsService.mapDispatchToThisForm({
-      uploadFile: model => dispatch(this.UploadsService.upload(model)),
+      uploadFile: model => dispatch(this.UploadsService.upload(model))
     })(dispatch);
   };
 
   private _uploadFile(file) {
-
     // console.log('modelName', modelName);
     // console.log('file', file);
     // console.log('entity', entity);
@@ -168,12 +205,16 @@ export class CampFormComponentController {
     }
 
     this.uploadFile({
-      categoria: 'ipotesi_spesa',
+      categoria: "ipotesi_spesa",
       file: file,
       model_id: this.model.id,
-      model_name: 'Camps',
+      model_name: "Camps"
     })
-      .then(({ sameRoute, upload }) => sameRoute && this.updateModel({ name: 'upload', value: upload, skipHash: true }))
+      .then(
+        ({ sameRoute, upload }) =>
+          sameRoute &&
+          this.updateModel({ name: "upload", value: upload, skipHash: true })
+      )
       .then(() => this.getFormData(this.model.id));
 
     return Promise.resolve(this.model);
